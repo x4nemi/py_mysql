dd36c80f59bfe2b9a1b973f9d2069225e8c2ccb3
Ximena <ximena.garcia7482@alumnos.udg.mx>
2021-03-22 19:43:03 -0600

    comentarios

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ bd = mysql.connector.connect(
     database = 'nopalito'
 )
 
-cursor = bd.cursor()
+cursor = bd.cursor() #lo que hará la conexión del
+                     #script y la base de datos
 
 while True:
     print("1) Agregar usuario")
@@ -14,14 +15,16 @@ while True:
     opcion = int(input("Opcion: "))
 
     if opcion == 1:
-        correo = input("Correo electronico: ")
-        contrasenia = input("Contrasenia: ")
+        valores = []
+        valores.append(input("Correo electronico: "))
+        valores.append(input("Contrasenia: "))
 
         consulta = "INSERT INTO usuario (correo, contrasenia) " \
-                    "VALUES (%s, %s)"
-        cursor.execute(consulta, (correo, contrasenia))
-        bd.commit()
-        if cursor.rowcount:
+                   "VALUES (%s, %s)"
+        cursor.execute(consulta, tuple(valores)) # esta es la orden
+        bd.commit() # esto es lo que lo aplica a la base
+        if cursor.rowcount: # atributo que dice los renglones modificados
+        # si hay renglones afectados sí se aplicó
             print("Se agregó usuario")
         else:
             print("Error al agregar usuario")
@@ -30,7 +33,7 @@ while True:
 
         cursor.execute(consulta)
         for row in cursor.fetchall():
-            print("id: ", row[0])
+            print("\nid: ", row[0])
             print("correo: ", row[1])
             print("contrasenia: ", row[2])
     elif opcion == 0:
